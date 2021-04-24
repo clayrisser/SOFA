b29289169955820e9f49a4ed11c98145e56c4858
Clay Risser <email@clayrisser.com>
2021-04-24 02:03:49 -0500

    Improved graphback naming

--- src/common.ts
+++ src/common.ts
@@ -1,7 +1,14 @@
 import { paramCase } from 'param-case';
 
 export function convertName(name: string) {
-  return paramCase(name);
+  name = paramCase(name);
+  name = name.replace(/^create-/g, '');
+  name = name.replace(/^delete-/g, '');
+  name = name.replace(/^find-/g, '');
+  name = name.replace(/^get-/g, '');
+  name = name.replace(/^mutation-/g, '');
+  name = name.replace(/^update-/g, '');
+  return name;
 }
 
 export function isNil<T>(val: T) {
